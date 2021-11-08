8c8808d87c35046a4a8ff1a0fd422cdfac43344a
figoagil <54571578+figoagil@users.noreply.github.com>
2021-11-08 08:44:54 +0700

    Update kel3.py
    fix

--- kel3.py
+++ kel3.py
@@ -2,15 +2,17 @@ from experta import *
 class Loads(KnowledgeEngine):
  @DefFacts()
  def _initial_action(self):
+    global hasil 
     yield Fact(Problem="daging")
- print(" Expert System".center(80, "*"))
- print(" Pemilihan Jenis Potongan Daging ".center(80, "*"))
- print(" Kelompok 3 II4402 ".center(80, "*"))
- print(" 18218014 Novindra Nurrosyid Al Haqi ".center(80, "*"))
- print(" 18218030 Figo Agil Alunjati ".center(80, "*"))
- print(" 18218037 Wita Dewisari Tasya ".center(80, "*"))
- print("".center(80, "*"))
- print("".center(80, "*"))
+    hasil=""
+#  print(" Expert System".center(80, "*"))
+#  print(" Pemilihan Jenis Potongan Daging ".center(80, "*"))
+#  print(" Kelompok 3 II4402 ".center(80, "*"))
+#  print(" 18218014 Novindra Nurrosyid Al Haqi ".center(80, "*"))
+#  print(" 18218030 Figo Agil Alunjati ".center(80, "*"))
+#  print(" 18218037 Wita Dewisari Tasya ".center(80, "*"))
+#  print("".center(80, "*"))
+#  print("".center(80, "*"))
 
  #Rule 0
  @Rule(Fact(Problem='daging'), NOT(Fact(Q1=W())))
@@ -19,121 +21,168 @@ class Loads(KnowledgeEngine):
  #Rule 1
  @Rule(Fact(Problem='daging'), (Fact(Q1='sei sapi')))
  def diagnosis_1(self):
-    print("Anda butuh potongan daging neck")
+    global hasil
+    hasil ="Anda butuh potongan daging neck"
 #Rule 2
  @Rule(Fact(Problem='daging'), (Fact(Q1='daging asap')))
  def diagnosis_2(self):
-    print("Anda butuh potongan daging neck")
+    global hasil
+    hasil ="Anda butuh potongan daging neck"
 #Rule 3
  @Rule(Fact(Problem='daging'), (Fact(Q1='sup')))
  def diagnosis_3(self):
-    print("Anda butuh potongan daging brisket")
+    global hasil
+    hasil ="Anda butuh potongan daging brisket"
 #Rule 4
  @Rule(Fact(Problem='daging'), (Fact(Q1='rawon')))
  def diagnosis_4(self):
-    print("Anda butuh potongan daging brisket")
+    global hasil
+    hasil ="Anda butuh potongan daging brisket"
 #Rule 5
  @Rule(Fact(Problem='daging'), (Fact(Q1='soto')))
  def diagnosis_5(self):
-    print("Anda butuh potongan daging brisket")
+    global hasil
+    hasil ="Anda butuh potongan daging brisket"
 #Rule 6
  @Rule(Fact(Problem='daging'), (Fact(Q1='bakso')))
  def diagnosis_6(self):
-    print("Anda butuh potongan daging chuck")
+    global hasil
+    hasil ="Anda butuh potongan daging chuck"
 #Rule 7
  @Rule(Fact(Problem='daging'), (Fact(Q1='semur')))
  def diagnosis_7(self):
-    print("Anda butuh potongan daging chuck")
+    global hasil
+    hasil ="Anda butuh potongan daging chuck"
 #Rule 8
  @Rule(Fact(Problem='daging'), (Fact(Q1='steak')))
  def diagnosis_8(self):
-    print("Anda butuh potongan daging tenderloin")
+    global hasil
+    hasil ="Anda butuh potongan daging tenderloin"
 #Rule 9
  @Rule(Fact(Problem='daging'), (Fact(Q1='sup iga')))
  def diagnosis_9(self):
-    print("Anda butuh potongan daging rib")
+    global hasil
+    hasil ="Anda butuh potongan daging rib"
 #Rule 10
  @Rule(Fact(Problem='daging'), (Fact(Q1='iga bakar')))
  def diagnosis_10(self):
-    print("Anda butuh potongan daging tenderloin")
+    global hasil
+    hasil ="Anda butuh potongan daging tenderloin"
 #Rule 11
  @Rule(Fact(Problem='daging'), (Fact(Q1='konro')))
  def diagnosis_11(self):
-    print("Anda butuh potongan daging rib")
+    global hasil
+    hasil ="Anda butuh potongan daging rib"
 #Rule 12
  @Rule(Fact(Problem='daging'), (Fact(Q1='yakiniku')))
  def diagnosis_12(self):
-    print("Anda butuh potongan daging sirloin")
+    global hasil
+    hasil ="Anda butuh potongan daging sirloin"
 #Rule 13
  @Rule(Fact(Problem='daging'), (Fact(Q1='shabu-shabu')))
  def diagnosis_13(self):
-    print("Anda butuh potongan daging sirloin")
+    global hasil
+    hasil ="Anda butuh potongan daging sirloin"
 #Rule 14
  @Rule(Fact(Problem='daging'), (Fact(Q1='daging cincang')))
  def diagnosis_14(self):
-    print("Anda butuh potongan daging short plate")
+    global hasil
+    hasil ="Anda butuh potongan daging short plate"
 #Rule 15
  @Rule(Fact(Problem='daging'), (Fact(Q1='kornet')))
  def diagnosis_15(self):
-    print("Anda butuh potongan daging short plate")
+    global hasil
+    hasil ="Anda butuh potongan daging short plate"
 #Rule 16
  @Rule(Fact(Problem='daging'), (Fact(Q1='bacon sapi')))
  def diagnosis_16(self):
-    print("Anda butuh potongan daging short plate")
+    global hasil
+    hasil ="Anda butuh potongan daging short plate"
 #Rule 17
  @Rule(Fact(Problem='daging'), (Fact(Q1='sate')))
  def diagnosis_17(self):
-    print("Anda butuh potongan daging rump cap")
+    global hasil
+    hasil ="Anda butuh potongan daging rump cap"
 #Rule 18
  @Rule(Fact(Problem='daging'), (Fact(Q1='abon')))
  def diagnosis_18(self):
-    print("Anda butuh potongan daging rump cap")
+    global hasil
+    hasil ="Anda butuh potongan daging rump cap"
 #Rule 19
  @Rule(Fact(Problem='daging'), (Fact(Q1='oseng daging')))
  def diagnosis_19(self):
-    print("Anda butuh potongan daging rump cap")
+    global hasil
+    hasil ="Anda butuh potongan daging rump cap"
 #Rule 20
  @Rule(Fact(Problem='daging'), (Fact(Q1='rendang')))
  def diagnosis_20(self):
-    print("Anda butuh potongan daging round")
+    global hasil
+    hasil ="Anda butuh potongan daging round"
 #Rule 21
  @Rule(Fact(Problem='daging'), (Fact(Q1='dendeng')))
  def diagnosis_21(self):
-    print("Anda butuh potongan daging round")
+    global hasil
+    hasil ="Anda butuh potongan daging round"
 #Rule 22
  @Rule(Fact(Problem='daging'), (Fact(Q1='tumis lidah')))
  def diagnosis_22(self):
-    print("Anda butuh potongan daging tenderloin")
+    global hasil
+    hasil ="Anda butuh potongan daging tenderloin"
 #Rule 23
  @Rule(Fact(Problem='daging'), (Fact(Q1='bahan aditif')))
  def diagnosis_23(self):
-    print("Anda butuh potongan daging pink slime")
+    global hasil
+    hasil ="Anda butuh potongan daging pink slime"
 #Rule 24
  @Rule(Fact(Problem='daging'), (Fact(Q1='empal')))
  def diagnosis_24(self):
-    print("Anda butuh potongan daging round")
+    global hasil
+    hasil ="Anda butuh potongan daging round"
 #Rule 25
  @Rule(Fact(Problem='daging'), (Fact(Q1='bakso urat')))
  def diagnosis_25(self):
-    print("Anda butuh potongan daging shank")
+    global hasil
+    hasil ="Anda butuh potongan daging shank"
 #Rule 26
  @Rule(Fact(Problem='daging'), (Fact(Q1='kaldu')))
  def diagnosis_26(self):
-    print("Anda butuh potongan daging oxtail")
+    global hasil
+    hasil ="Anda butuh potongan daging oxtail"
 #Rule 27
  @Rule(Fact(Problem='daging'), (Fact(Q1='buntut bakar')))
  def diagnosis_27(self):
-    print("Anda butuh potongan daging oxtail")
+    global hasil
+    hasil ="Anda butuh potongan daging oxtail"
 #Rule 28
  @Rule(Fact(Problem='daging'), (Fact(Q1='sate padang')))
  def diagnosis_28(self):
-    print("Anda butuh potongan daging tongue")
+    global hasil
+    hasil = "Anda butuh potongan daging tongue"
 #Rule 29
  @Rule(Fact(Problem='daging'), (Fact(Q1='lidah goreng')))
  def diagnosis_29(self):
-    print("Anda butuh potongan daging tongue")
+    global hasil
+    hasil = "Anda butuh potongan daging tongue"
 
-engine = Loads()
-engine.reset()
-engine.run()
-input('press enter to exit')
+def get_daging(Problem_input, Q1_input):
+    global hasil
+    hasil=""
+    engine = Loads()
+    engine.reset()
+    engine.declare(
+        Fact(Problem = Problem_input), 
+        Fact(Q1 = Q1_input))
+    engine.run()
+    return hasil
+
+def get_masakan(Problem_input, Q1_input):
+    global hasil
+    hasil=""
+    engine.reset()
+    engine.declare(
+        Fact(Problem = Problem_input), 
+        Fact(Q1 = Q1_input))
+    engine.run()
+    print(hasil)
+    print(engine.facts)
+    return hasil
