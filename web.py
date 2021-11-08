2ed5b01907a4abb600d89d42b676887a1e0e8bd3
figoagil <54571578+figoagil@users.noreply.github.com>
2021-11-08 08:45:34 +0700

    Update web.py
    fix

--- web.py
+++ web.py
@@ -1,9 +1,19 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from kel3 import *
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return render_template('web2.html')
+@app.route("/", methods=['GET','POST'])
+def submit():
+    Problem_input = request.form.get("Problem")
+    Q1_input = request.form.get("Q1")
 
+    global hasil
+    hasil = get_daging(Problem_input, Q1_input)
+    if request.method == 'POST':
+        submitted = True
+    else:
+        submitted = False
+    return render_template('web2.html', result=hasil, submitted=submitted)    
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
